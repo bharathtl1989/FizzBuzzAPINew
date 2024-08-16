6d5e4fecd8f13b7c529e313d4df2fcfe3e99935c
Bharth TL <bharathtl.1989@gmail.com>
2024-08-16 13:31:02 +0530

    Made changes to accept Factory pattern in the Test Cases.

--- FizzBuzzAPI/FizzBuzzAPI.Tests/FizzBuzzControllerTests.cs
+++ FizzBuzzAPI/FizzBuzzAPI.Tests/FizzBuzzControllerTests.cs
@@ -1,15 +1,15 @@
 using FizzBuzzAPI.Controllers;
-using NUnit.Compatibility;
-using NUnit.Framework;
-using FizzBuzzAPI.Services.Interface;
-using Microsoft.AspNetCore.Mvc;
 using Moq;
+using Microsoft.AspNetCore.Mvc;
+using FizzBuzzAPI.Services.Interface;
+using FizzBuzzAPI.Factory.Interface;
 
 namespace FizzBuzzAPI.Tests
 {
     [TestFixture]
     public class FizzBuzzControllerTests
     {
+        private Mock<IFizzBuzzServiceFactory> _mockFizzBuzzServiceFactory;
         private Mock<IFizzBuzzService> _mockFizzBuzzService;
         private FizzBuzzController _controller;
 
@@ -17,76 +17,76 @@ namespace FizzBuzzAPI.Tests
         public void Setup()
         {
             _mockFizzBuzzService = new Mock<IFizzBuzzService>();
-            _controller = new FizzBuzzController(_mockFizzBuzzService.Object);
+            _mockFizzBuzzServiceFactory = new Mock<IFizzBuzzServiceFactory>();
+
+            // Setup the factory to return the mock service
+            _mockFizzBuzzServiceFactory.Setup(factory => factory.CreateFizzBuzzService())
+                                       .Returns(_mockFizzBuzzService.Object);
+
+            _controller = new FizzBuzzController(_mockFizzBuzzServiceFactory.Object);
         }
 
+
         [Test]
-        public void GetFizzBuzz_ValidNumber_ReturnsExpectedResult()
+        public void GetFizzBuzz_ValidList_ReturnsExpectedResults()
         {
             // Arrange
-            int input = 15;
-            string expected = "FizzBuzz";
-            _mockFizzBuzzService.Setup(service => service.GetFizzBuzzResult(input)).Returns(expected);
+            var inputs = new List<string> { "3", "5", "15", "7" };
+            var expected = new List<string> { "Fizz", "Buzz", "FizzBuzz", "Divided 7 by 3\nDivided 7 by 5" };
+
+            _mockFizzBuzzService.Setup(service => service.GetFizzBuzzResult(inputs)).Returns(expected);
 
             // Act
-            var result = _controller.GetFizzBuzz(input.ToString()) as OkObjectResult;
+            var result = _controller.GetFizzBuzz(inputs) as OkObjectResult;
 
             // Assert
-            Assert.IsNotNull(result);
-            Assert.AreEqual(200, result.StatusCode);
-            Assert.AreEqual(expected, result.Value);
+            Assert.Equals(200, result.StatusCode);
         }
         [Test]
-        public void GetFizzBuzz_AlphabeticInput_ReturnsBadRequest()
-        {
-            // Act
-            var result = _controller.GetFizzBuzz("abc") as BadRequestObjectResult;
-
-            // Assert
-            Assert.IsNotNull(result);
-            Assert.AreEqual(400, result.StatusCode);
-            Assert.AreEqual("Invalid input. Alphabetic characters are not allowed. Please enter a valid number.", result.Value);
-        }
-
-        [Test]
-        public void GetFizzBuzz_EmptyInput_ReturnsBadRequest()
-        {
-            // Act
-            var result = _controller.GetFizzBuzz("") as BadRequestObjectResult;
-
-            // Assert
-            Assert.IsNotNull(result);
-            Assert.AreEqual(400, result.StatusCode);
-            Assert.AreEqual("Input cannot be empty. Please enter a valid number.", result.Value);
-        }
-
-        [Test]
-        public void GetFizzBuzz_NonNumericInput_ReturnsBadRequest()
-        {
-            // Act
-            var result = _controller.GetFizzBuzz("123.45") as BadRequestObjectResult;
-
-            // Assert
-            Assert.IsNotNull(result);
-            Assert.AreEqual(400, result.StatusCode);
-            Assert.AreEqual("Invalid input. Please enter a valid number.", result.Value);
-        }
-
-        [Test]
-        public void GetFizzBuzz_ValidInput_CallsFizzBuzzService()
+        public void GetFizzBuzz_EmptyList_ReturnsBadRequest()
         {
             // Arrange
-            int input = 3;
-            _mockFizzBuzzService.Setup(service => service.GetFizzBuzzResult(input)).Returns("Fizz");
+            var inputs = new List<string>();
 
             // Act
-            var result = _controller.GetFizzBuzz(input.ToString()) as OkObjectResult;
+            var result = _controller.GetFizzBuzz(inputs) as BadRequestObjectResult;
 
             // Assert
-            _mockFizzBuzzService.Verify(service => service.GetFizzBuzzResult(input), Times.Once);
-            Assert.IsNotNull(result);
-            Assert.AreEqual(200, result.StatusCode);
-            Assert.AreEqual("Fizz", result.Value);
+            Assert.Equals(400, result.StatusCode);
+            Assert.Equals("Input cannot be empty. Please provide a list of numbers.", result.Value);
         }
+        [Test]
+        public void GetFizzBuzz_ListWithInvalidEntries_ReturnsExpectedResults()
+        {
+            // Arrange
+            var inputs = new List<string> { "3", "abc", "15", "" };
+            var expected = new List<string> { "Fizz", "Invalid input", "FizzBuzz", "Invalid input" };
+
+            _mockFizzBuzzService.Setup(service => service.GetFizzBuzzResult(inputs)).Returns(expected);
+
+            // Act
+            var result = _controller.GetFizzBuzz(inputs) as OkObjectResult;
+
+            // Assert
+            Assert.Equals(200, result.StatusCode);
+            Assert.Equals(expected, result.Value);
+        }
+        [Test]
+        public void GetFizzBuzz_NumberNotDivisibleBy3Or5_ReturnsDividedByMessages()
+        {
+            // Arrange
+            var inputs = new List<string> { "7" };
+            var expected = new List<string> { "Divided 7 by 3\nDivided 7 by 5" };
+
+            _mockFizzBuzzService.Setup(service => service.GetFizzBuzzResult(inputs)).Returns(expected);
+
+            // Act
+            var result = _controller.GetFizzBuzz(inputs) as OkObjectResult;
+
+            // Assert
+            Assert.Equals(200, result.StatusCode);
+            Assert.Equals(expected, result.Value);
+        }
+
     }
 }
